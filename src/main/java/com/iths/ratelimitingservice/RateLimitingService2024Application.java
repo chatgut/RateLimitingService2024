17cc2626c4601c05d1555ed9c8e18cc3fc41346a
Daniel Backberg <daniel.backberg@iths.se>
2024-05-07 06:26:32 +0200

    Initial commit

---
+++ src/main/java/com/iths/ratelimitingservice/RateLimitingService2024Application.java
@@ -0,0 +1,13 @@
+package com.iths.ratelimitingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateLimitingService2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RateLimitingService2024Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/iths/ratelimitingservice/RateLimitingService2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iths.ratelimitingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateLimitingService2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
